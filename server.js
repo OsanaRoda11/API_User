b72ada09121ce465fcf8f20a1a6990af8d94e0d0
Osana Manuel <osanaroda00@gmail.com>
2025-05-06 20:17:33 +0100

    Criação de novas rotas

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 import express, { request, response } from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/usuario', (request, response) =>{
-    response.send("Ok, deu certo!")
+const users = []
+
+app.post('/usuarios', (request, response) => {
+
+    users.push(request.body)
+    
+    response.status(201).json(request.body)
+
+    response.send('Ok, tudo bem!!!!')
+})
+
+app.get('/usuarios', (request, response) =>{
+    response.status(200).json(users)
 })
 
 app.listen(3000)
+
+
+
